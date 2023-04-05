d781a261a71b6b8e01fad47ae95b15a685f28155
Dawid Kocik <kocik.dawid@wp.pl>
2023-04-05 17:20:52 +0200

    commit

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -13,39 +13,31 @@ public class Main {
         Path path = Paths.get(myObj.getPath());
         BasicFileAttributes attr;
         try {
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-
-                attr = Files.readAttributes(path, BasicFileAttributes.class);
-                // SimpleDateFormat sd = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-                SimpleDateFormat hoursSDF = new SimpleDateFormat("HH");
+            boolean createFile = myObj.createNewFile();
+            attr = Files.readAttributes(path, BasicFileAttributes.class);
+            // SimpleDateFormat sd = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+             SimpleDateFormat hoursSDF = new SimpleDateFormat("HH");
                 int returnHours = Integer.parseInt(hoursSDF.format(attr.creationTime().toMillis()));
-                System.out.println("Hours of creating file: " + returnHours);
-
                 String filename = myObj.getName();
-                if(filename.contains("xml"))
-                    if(myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\HOME\\filename.xml")))
-                        System.out.println("File moved to HOME successfully");
 
-                    if (returnHours % 2 == 0) {
-                        if (myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\DEV\\filename.jar"))) {
-                            System.out.println("File moved DEV successfully");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
-                    } else{
+                if(createFile && filename.contains(".xml")) {
+                    if (myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\HOME\\" + myObj)))
+                        System.out.println("File moved to to the HOME successfully");
+                    else if(myObj.exists())
+                        System.out.println("The same file exists already in the HOME folder -- > " + myObj.delete());
+                }
+                else if (createFile && returnHours % 2 == 0 && filename.contains(".jar")) {
+                    if (myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\DEV\\" + myObj)))
+                        System.out.println("File moved to the DEV successfully");
+                    else if(myObj.exists())
+                        System.out.println("The same file exists already in the DEV folder -- > " + myObj.delete());
 
-                        if( myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\TEST\\filename.jar"))) {
-                            System.out.println("File moved TEST successfully");
-                        } else {
-                            System.out.println("Failed to move the file");
-                        }
-                    }
-
-            } else {
-                System.out.println("File already exists.");
-            }
-
+                } else if(createFile && returnHours % 2 != 0 && filename.contains(".jar")) {
+                    if(myObj.renameTo(new File("C:\\Users\\kocik\\OneDrive\\Dokumenty\\Programowanie\\Java\\Small project\\TEST\\" + myObj)))
+                        System.out.println("File moved to the TEST successfully");
+                    else if(myObj.exists())
+                        System.out.println("The same file exists already in the TEST folder -- > " + myObj.delete());
+                }
         } catch (IOException e) {
             System.out.println("oops error! " + e.getMessage());
         }
